8c707d597030281d52d34be828bd3c8211b77e80
eriz <471295986@qq.com>
2019-01-28 14:45:51 +0800

    add 拦截器

---
+++ src/main/java/com/eriz/common/filter/BaseInterceptor.java
@@ -0,0 +1,31 @@
+package com.eriz.common.filter;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 系统拦截器
+ *
+ * @author eriz
+ * @date 2019年1月25日
+ */
+public class BaseInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        System.out.println("preHandle===============================");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        System.out.println("postHandle===============================");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        System.out.println("afterCompletion===============================");
+    }
+}
--- src/main/java/com/eriz/common/filter/WebConfig.java
+++ src/main/java/com/eriz/common/filter/WebConfig.java
@@ -3,6 +3,8 @@ package com.eriz.common.filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * 系统web请求配置
@@ -11,18 +13,26 @@ import org.springframework.context.annotation.Configuration;
  * @date 2019年1月24日
  */
 @Configuration
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
 
     /**
      * 配置系统过滤器
      */
     @SuppressWarnings({"rawtypes", "unchecked"})
     @Bean
-    public FilterRegistrationBean filterRegist() {
+    public FilterRegistrationBean filterRegister() {
         FilterRegistrationBean frBean = new FilterRegistrationBean();
         frBean.setFilter(new BaseFilter());
         // 过滤拦截器
         frBean.addUrlPatterns("/sys/user/*");
         return frBean;
+    }
+
+    /**
+     * 配置系统过滤器
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new BaseInterceptor()).addPathPatterns("/sys/user/*");
     }
 }
